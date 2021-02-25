b35eca8bbd5dcc9df1884cb21c37d02bb2e60e99
Claire Bullock <bullockcm13@gmail.com>
2021-02-25 19:17:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,13 +24,3 @@ end
   
   
   
-  
-  
-  
-  
-  
-  
-  
-#   If it's before noon, greet the user with "Good Morning!"
-  
-# If it's after noon, greet the user with "Good Afternoon!". Remember that getting the time may return results in 24 hour time! Anytime on or after 12 is the afternoon.
